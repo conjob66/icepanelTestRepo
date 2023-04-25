ceb31f3fdd3631fc14a447fa78271628bd5d2a8c
Patrik Paldi <ppd2bp@bosch.com>
2023-04-25 13:07:41 +0200

    init project

---
+++ ModelMapping/App/Component/AutoMapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModelMapping.App.Component
+{
+    public class AutoMapping
+    {
+        public string FilePath { get; set; }
+        public List<Channel> ChannelList { get; set; }
+        public AutoMapping() { }
+        public void BuildFromFile(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public void SearchPairs()
+        {
+
+        }
+    }
+}
---
+++ ModelMapping/App/Component/Channel.cs
@@ -0,0 +1,10 @@
+﻿namespace ModelMapping.App.Component
+{
+    public class Channel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Channel() { }
+        public Channel(string name, string description) { Name = name; Description = description; }
+    }
+}
